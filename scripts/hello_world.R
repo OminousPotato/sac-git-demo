be104b8dec030088ea1a091c3024260cab10e6d1
Brian Yngve <brianyngve@gmail.com>
2024-10-09 18:18:10 -0400

    Made rough draft of a chart

--- scripts/hello_world.R
+++ scripts/hello_world.R
@@ -1,2 +1,16 @@
 print("Hello World")
 
+library(tid)
+
+standings <- read.csv("data/standings.csv") 
+
+standings_head <- standings %>% head
+
+ggplot(data = standings_head, aes(x = Tm, y = W)) +
+  geom_col() + 
+  labs(
+    x = "Teams",
+    y = "Wins",
+    title = "Top MLB Team Win Counts",
+    caption = "Data from Baseball Reference"
+  )
